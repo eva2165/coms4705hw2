982c31dd4903b03040efcf20c95a21f34874bdce
Eva T. <98779597+eva2165@users.noreply.github.com>
2024-04-08 17:17:24 -0400

    Write full submission

--- cky.py
+++ cky.py
@@ -8,6 +8,7 @@ import sys
 from collections import defaultdict
 import itertools
 from grammar import Pcfg
+import copy
 
 ### Use the following two functions to check the format of your data structures in part 3 ###
 def check_table_format(table):
@@ -89,32 +90,110 @@ class CkyParser(object):
         Initialize a new parser instance from a grammar. 
         """
         self.grammar = grammar
+        
+    
+    def parse(self, tokens):
+        
+        self.tokens = tokens
+        self.table = dict()
+        self.probs = dict()
+        
+        # Recursive call that accesses self.table,probs,tokens
+        self.__parse__((0, len(self.tokens)))
+        
+        # assert check_table_format(self.table)
+        # assert check_probs_format(self.probs)
+        
+        table = copy.deepcopy(self.table)
+        probs = copy.deepcopy(self.probs)
+        
+        self.tokens, self.table, self.probs = None, None, None
+        
+        return table, probs
+    
+    def __parse__(self, span):
+        
+        # We can skip table[span]s that have already been parsed, because
+        # the table is built from the bottom up
+        try:
+            self.table[span]
+            return
+        except KeyError:
+            self.table[span] = dict()
+            self.probs[span] = dict()
+        
+        if span[1] - span[0] == 1:
+            terminal = self.tokens[span[0]]
+            for rule in self.grammar.rhs_to_rules[(terminal,)]:
+                self.table[span][rule[0]] = terminal
+                self.probs[span][rule[0]] = math.log(rule[2])
+            return
+        
+        for split in range(span[0] + 1, span[1]):
+            
+            leftspan = (span[0], split)
+            rightspan = (split, span[1])
+            
+            self.__parse__(leftspan)
+            self.__parse__(rightspan)
+            
+            for leftlhs in self.table[leftspan].keys():
+                for rightlhs in self.table[rightspan].keys():
+                    for rule in self.grammar.rhs_to_rules[(leftlhs, rightlhs)]:
+                        prob = math.log(rule[2]) \
+                            + self.probs[leftspan][leftlhs] \
+                            + self.probs[rightspan][rightlhs]
+                        try:
+                            if prob > self.probs[span][rule[0]]:
+                                # Overwrite backtrace
+                                self.table[span][rule[0]] = \
+                                    ((leftlhs, leftspan[0], leftspan[1]), \
+                                     (rightlhs, rightspan[0], rightspan[1]))
+                                self.probs[span][rule[0]] = prob
+                        except KeyError:
+                            # Write backtrace (no key rule[0] in dict)
+                            self.table[span][rule[0]] = \
+                                ((leftlhs, leftspan[0], leftspan[1]), \
+                                 (rightlhs, rightspan[0], rightspan[1]))
+                            self.probs[span][rule[0]] = prob
 
-    def is_in_language(self,tokens):
+    def is_in_language(self, tokens):
         """
         Membership checking. Parse the input tokens and return True if 
         the sentence is in the language described by the grammar. Otherwise
         return False
         """
-        # TODO, part 2
-        return False 
+        table, _ = self.parse(tokens)
+        if self.grammar.startsymbol in table[(0, len(tokens))]:
+            return True
+        else:
+            return False
        
     def parse_with_backpointers(self, tokens):
         """
         Parse the input tokens and return a parse table and a probability table.
         """
-        # TODO, part 3
-        table= None
-        probs = None
-        return table, probs
+        return self.parse(tokens)
+    
+    # For debugging
+    def get_table(self, tokens):
+        table, _ = self.parse(tokens)
+        return table
 
 
 def get_tree(chart, i,j,nt): 
     """
     Return the parse-tree rooted in non-terminal nt and covering span i,j.
     """
-    # TODO: Part 4
-    return None 
+    if i + 1 == j:
+        return (nt, chart[(i,j)][nt])
+    
+    return (nt, get_tree(chart, chart[(i,j)][nt][0][1], \
+                                chart[(i,j)][nt][0][2], \
+                                chart[(i,j)][nt][0][0]), \
+                get_tree(chart, chart[(i,j)][nt][1][1], \
+                                chart[(i,j)][nt][1][2], \
+                                chart[(i,j)][nt][1][0]))
  
        
 if __name__ == "__main__":
--- grammar.py
+++ grammar.py
@@ -6,7 +6,11 @@ Daniel Bauer
 
 import sys
 from collections import defaultdict
-from math import fsum
+# from math import fsum
+import math
+
+def rule_to_str(rule):
+    return ' -> '.join([rule[0], ' '.join(rule[1])])
 
 class Pcfg(object): 
     """
@@ -16,8 +20,9 @@ class Pcfg(object):
     def __init__(self, grammar_file): 
         self.rhs_to_rules = defaultdict(list)
         self.lhs_to_rules = defaultdict(list)
-        self.startsymbol = None 
-        self.read_rules(grammar_file)      
+        self.startsymbol = None
+        self.verimsg = 'FLAG_UNSET'
+        self.read_rules(grammar_file)
  
     def read_rules(self,grammar_file):
         
@@ -47,11 +52,47 @@ class Pcfg(object):
         Return True if the grammar is a valid PCFG in CNF.
         Otherwise return False. 
         """
-        # TODO, Part 1
-        return False 
+        
+        nonterminals = self.lhs_to_rules.keys()
+        
+        for rules in self.lhs_to_rules.values():
+            total_prob = 0.0
+            for rule in rules:
+                if len(rule[1]) == 2:
+                    # First possible form
+                    if rule[1][0] not in nonterminals:
+                        self.verimsg = rule_to_str(rule) + \
+                            " has terminal in RHS: " + rule[1][0]
+                        return False
+                    if rule[1][1] not in nonterminals:
+                        self.verimsg = rule_to_str(rule) + \
+                            " has terminal in RHS: " + rule[1][1]
+                        return False
+                elif len(rule[1]) == 1:
+                    # Second possible form
+                    if rule[1][0] in nonterminals:
+                        self.verimsg = rule_to_str(rule) + \
+                            " has nonterminal in RHS: " + rule[1][0]
+                        return False
+                else:
+                    # Not in correct form
+                    self.verimsg = rule_to_str(rule) + \
+                        " in non-CNF form"
+                    return False
+                total_prob += rule[2]
+            if not math.isclose(total_prob, 1.0):
+                self.verimsg = "total probability of all LHS = " + \
+                    rules[0][0] + " is " + str(total_prob)
+                return False
+        
+        return True
 
 
 if __name__ == "__main__":
     with open(sys.argv[1],'r') as grammar_file:
         grammar = Pcfg(grammar_file)
+        if grammar.verify_grammar():
+            print(f"{sys.argv[1]} is a valid PCFG in CNF")
+        else:
+            print(f"{sys.argv[1]} is an invalid grammar: {grammar.verimsg}")
         
